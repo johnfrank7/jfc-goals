d0142465818bf464b9d34f4d645ee62690650ccb
John Franz O. Caliguid <johnfranzcaliguid7@gmail.com>
2025-08-27 09:46:31 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTPcE1G_tVZdQeQWsbfVHzjVpEOlECxZ4",
+  authDomain: "jfc-goals.firebaseapp.com",
+  projectId: "jfc-goals",
+  storageBucket: "jfc-goals.firebasestorage.app",
+  messagingSenderId: "528883077998",
+  appId: "1:528883077998:web:f9112a84d787a23311e77f"
+};
+
+
+const app = initializeApp(firebaseConfig);
